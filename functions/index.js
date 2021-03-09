b809f8b94cb928c8c8161aa5131959833189e02d
lukevink <luke@lukevink.com>
2021-03-09 11:52:52 +0100

    Commit base files

---
+++ functions/index.js
@@ -0,0 +1,53 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.newUserSignup = functions.auth.user().onCreate((user) => {
+  admin.firestore().collection("users").doc(user.uid).set({
+    email: user.email,
+    profileImageUrl: user.photoURL,
+    displayName: user.displayName,
+    lists: [],
+    isAdmin: false,
+  });
+});
+
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+  const doc = admin.firestore().collection("users").doc(user.uid);
+  return doc.delete();
+});
+
+
+// // http request
+// exports.randomNumber = functions.https.onRequest((request, response) => {
+//   const number = Math.round(Math.random()*100);
+//   console.log(number);
+//   response.send(number.toString());
+// });
+
+// // callable function
+// exports.sayHello = functions.https.onCall((data, context) => {
+//   if (data.name) {
+//     return `hello ${data.name}`;
+//   }
+//   return "hello, ninjas";
+// });
+
+
+exports.getAllUsers = functions.https.onRequest((req, res) => {
+  const allUsers = [];
+  return admin.auth().listUsers()
+      .then(function(listUsersResult) {
+        listUsersResult.users.forEach(function(userRecord) {
+          // For each user
+          const userData = userRecord.toJSON();
+          allUsers.push(userData);
+        });
+        res.status(200).send(JSON.stringify(allUsers));
+      })
+      .catch(function(error) {
+        console.log("Error listing users:", error);
+        res.status(500).send(error);
+      });
+});
---
+++ public/js/auth.js
@@ -0,0 +1,88 @@
+
+// const loginForm = document.querySelector('.form-login');
+// const loginForm = document.querySelector('.form-login');
+const googleBtn = document.querySelector('#googleBtn');
+
+// Login form
+// loginForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//
+//       const email = loginForm.email.value;
+//       const password = loginForm.password.value;
+//
+//       firebase.auth().signInWithEmailAndPassword(email, password)
+//         .then((user) => {
+//           console.log('logged in', user);
+//           loginForm.reset();
+//         })
+//         .catch((error) => {
+//           console.log(error.message);
+//           loginForm.querySelector('#error-message').textContent = error.message;
+//         });
+// });
+
+googleBtn.addEventListener('click', (e) => {
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+
+    // Handle Errors here.
+      var errorCode = error.code;
+      console.log(errorCode);
+      alert(errorCode);
+
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      alert(errorMessage);
+      });
+
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        window.location.href = '../';
+      }
+    });
+
+});
+
+// registerForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//
+//       const email = loginForm.email.value;
+//       const password = loginForm.password.value;
+//
+//       firebase.auth().createUserWithEmailAndPassword(email, password)
+//         .then((user) => {
+//           console.log('registered', user);
+//           loginForm.reset();
+//         })
+//         .catch((error) => {
+//           console.log(error.message);
+//         });
+// });
